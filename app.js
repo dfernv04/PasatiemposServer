569f4dac6f6323de2d31d6b85a02cf148367c0f9
dfernv04 <dfernv04@estudiantes.unileon.es>
2021-05-13 00:12:17 +0200

    Avances en el servidor

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var livereload = require("livereload");
+var connectLivereload = require("connect-livereload");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const publicDirectory =  path.join(__dirname, '/public');
+
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicDirectory);
 
 var app = express();
 
@@ -12,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicDirectory));
+app.use(connectLivereload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -20,5 +28,18 @@ app.use('/users', usersRouter);
 /* Aqui se hace todo*/
 console.log("Servidor en puerto: 3000");
 
+// error handler
+app.use((error, req, res, next) => {
+    if (!error.statusCode) error.statusCode = 500;
+
+    if (error.statusCode === 404) {
+        console.log(`${req.ip} tried to access ${req.originalUrl}`)
+        return res.status(404).redirect('error.html');
+
+    }
+    return res
+        .status(error.statusCode)
+        .json({ error: error.toString() });
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html', { title: 'Express' });
 });
 
+/* GET no indexed page */
+router.get('*', function(req, res, next) {
+  const error = new Error('Page not found');
+  error.statusCode = 404;
+  next(error);
+});
+
 module.exports = router;
